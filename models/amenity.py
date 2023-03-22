917c205804ecae22d99320087c164cbcf6abd849
Amelianancy <ogbonnanancy11@gmail.com>
2023-03-22 22:13:15 +0000

    commit

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-from models.base_model import Base
-from sqlalchemy import Column
-from sqlalchemy import String
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from models import storage_type
+from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel, Base):
-    """creates an amenity class"""
+    '''amenity class'''
     __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity",viewonly=False)
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
