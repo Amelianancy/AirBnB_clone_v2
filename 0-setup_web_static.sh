3bbc03e58354157e924c047b1ce244c93d6d2c51
Torpeedofv <torpeedo.fv@gmail.com>
2023-04-09 14:47:44 +0100

    advanced task

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,3 +10,40 @@ ln -sf /data/web_static/releases/test/  /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i '/error_page 404 \/404\.html;/i \    location \/hbnb_static\/ {\n        alias \/data\/web_static\/current\/;\n    }' /etc/nginx/sites-enabled/default
 service nginx restart
+
+
+
+# implementing a script that sets up webservers for deployment
+package { 'nginx':
+  ensure   => 'present',
+  provider => 'apt',
+} ->
+file { ['/data', '/data/web_static', '/data/web_static/releases', '/data/web_static/releases/test', '/data/web_static/shared']:
+  ensure => 'directory',
+} ->
+file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test/',
+} ->
+file { '/data/web_static/releases/test/index.html':
+  ensure => 'present',
+  content => "Testing, testing!!!",
+} ->
+file { '/data/':
+  owner => $new_owner,
+  group => $new_group,
+} ->
+file_line { 'add_hbnb_static_location':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  line   => '    location /hbnb_static/ {\n        alias /data/web_static/current/;\n    }',
+  after  => 'error_page 404 /404.html;',
+  match  => '^error_page 404 /404\.html;$',
+} ->
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+} ->
+
+exec { 'nginx restart':
+  path => '/etc/init.d/'
+}
--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -3,8 +3,24 @@ package { 'nginx':
   ensure   => 'present',
   provider => 'apt',
 } ->
-file { ['/data', '/data/web_static', '/data/web_static/releases', '/data/web_static/releases/test', '/data/web_static/shared']:
-  ensure => 'directory',
+file { '/data':
+  ensure  => 'directory'
+} ->
+
+file { '/data/web_static':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases/test':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/shared':
+  ensure => 'directory'
 } ->
 file { '/data/web_static/current':
   ensure => 'link',
