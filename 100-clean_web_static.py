6b9b98e8272067d37fbd97af10793d23f50ecc2e
Torpeedofv <torpeedo.fv@gmail.com>
2023-04-21 10:56:09 +0100

    task 0

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 """a script that deletes out of date archives using the function
 do_clean"""
-do_deploy = __import__('3-deploy_web_static').do_deploy
+from fabric.api import local, put, run, env
+import os
+env.hosts = ['34.227.101.5', '52.91.116.255']
+env.user = 'ubuntu'
+env.identity = '~/.ssh/school'
+env.password = None
 
 
 def do_clean(number=0):
     """deletes out of date archives"""
-
+    
