31db69473a22c143f93e476c1aa544dc7ef23595
Torpeedofv <torpeedo.fv@gmail.com>
2023-04-21 12:52:03 +0100

    task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Starts a web application"""
+from flask import Flask, escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """returns hello hbnb"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """returns C followed by the vallued of text"""
+    new_text = escape(text).replace('_', ' ')
+    return "C %s" % new_text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
