25e98d7cec32b3cae1d128bdf616bd9c196a0e89
Torpeedofv <torpeedo.fv@gmail.com>
2023-04-26 14:35:19 +0100

    commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """A script that starts a flask web application"""
 from flask import Flask
-
-
 app = Flask(__name__)
+
+
 @app.route('/', strict_slashes=False)
 def home():
     """return a string from the ip and specified port"""
