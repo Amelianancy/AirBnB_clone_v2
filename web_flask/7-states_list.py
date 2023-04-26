f2a7661c6d1e5684e609c264653c08e6c9c213b7
Torpeedofv <torpeedo.fv@gmail.com>
2023-04-26 15:26:48 +0100

    commit

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,35 +1,10 @@
 #!/usr/bin/python3
 """A script that starts a web flask application"""
 
-"""This script starts a Flask web application"""
-
 
 from flask import Flask, render_template
 from models.state import State
 from models import storage
-
-app = Flask(__name__)
-
-
-@app.teardown_appcontext
-def shutdown_session(exception=None):
-    """ This function closes the session"""
-    storage.close()
-
-
-@app.route('/states_list')
-def show_states():
-    """ This function renders the template 7-states_list.html"""
-    states = storage.all(State).values()
-    return render_template('7-states_list.html', states=states)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
-"""from flask import Flask, render_template
-from models import storage
-from models.state import State
-
 
 app = Flask(__name__)
 
@@ -48,4 +23,4 @@ def states_list():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)"""
+    app.run(host='0.0.0.0', port=5000)
